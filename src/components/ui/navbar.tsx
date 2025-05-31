d4e8e61afac838e5ec7908eef5a1a1ec7b5e2311
Deba00407 <cdebanjan456@gmail.com>
2025-05-31 23:51:12 +0530

    Added app logos and updated the navbar

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -4,29 +4,40 @@ import { SignedOut, UserButton } from '@clerk/nextjs'
 import { SignedIn } from '@clerk/nextjs'
 import ThemeToggleButton from './themeToggleButton'
 import { usePathname } from 'next/navigation'
+import Image from 'next/image'
 
 const Navbar = () => {
     const pathname = usePathname()
     return (
         <>
-            <nav className="min-h-[50px] text-md flex items-center justify-center bg-transparent text-black dark:text-white">
+            <nav className="min-h-[50px] text-md flex items-center justify-between bg-transparent text-black dark:text-white px-4">
 
-                <SignedOut>
-                    <Link href={"/"} className={`mr-4 ${pathname === "/" ? "text-blue-500" : ""}`}>Landing Page</Link>
-                    <Link href={"/login"} className={`mr-4 ${pathname === "/login" ? "text-blue-500" : ""}`}>Login</Link>
-                    <Link href={"/register"} className={`mr-4 ${pathname === "/register" ? "text-blue-500" : ""}`}>Register</Link>
-                </SignedOut>
-
-
-                <div className='flex items-center mr-4 justify-center'>
-                    <SignedIn>
-                        <Link href={"/home"} className={`mr-4 ${pathname === "/home" ? "text-blue-500" : ""}`}>Home</Link>
-                        <UserButton />
-                    </SignedIn>
+                <div className="h-[50px] flex items-center justify-center px-2">
+                    <Image
+                        src="/logo.png"
+                        alt="Logo"
+                        height={100}
+                        width={100}
+                        className="h-full rounded-full object-cover"
+                    />
                 </div>
 
-                <ThemeToggleButton />
+
+                <div className="flex items-center justify-center gap-4">
+                    <SignedOut>
+                        <Link href="/login" className={`mr-4 ${pathname === "/login" ? "text-blue-500" : ""}`}>Login</Link>
+                        <Link href="/register" className={`mr-4 ${pathname === "/register" ? "text-blue-500" : ""}`}>Register</Link>
+                    </SignedOut>
+
+                    <SignedIn>
+                        <Link href="/home" className={`mr-4 ${pathname === "/home" ? "text-blue-500" : ""}`}>Home</Link>
+                        <UserButton />
+                    </SignedIn>
+
+                    <ThemeToggleButton />
+                </div>
             </nav>
+
         </>
     )
 }
